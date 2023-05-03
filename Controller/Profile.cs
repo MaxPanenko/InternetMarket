c2a00c93a6f0b5c34dfadc017b1819c25289e2f8
MaxPanenko <panenko123456789987654321@gmail.com>
2023-05-03 21:53:20 +0200

    Added Menu

--- Controller/Profile.cs
+++ Controller/Profile.cs
@@ -3,19 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
-namespace InternetMarket.Controller
+namespace InternetMarket.Controller 
 {
     public class Profile
     {
-        public List<User> Users { get; }   
-        
-        public Dictionary <User,User> Emails { get; }
+        public List<User> Users { get; }
 
         public User User { get; }
 
@@ -23,7 +22,7 @@ namespace InternetMarket.Controller
 
         public bool IsNewUser { get; }
 
-        public Profile(string userName) 
+        public Profile(string userName)
         {
             if (string.IsNullOrWhiteSpace(userName))
             {
@@ -33,20 +32,22 @@ namespace InternetMarket.Controller
 
             CurrentUser = Users.FirstOrDefault(u => u.Name == userName);
 
-            
-            
 
-            if (CurrentUser == null) 
-            {               
+
+
+            if (CurrentUser == null)
+            {
                 CurrentUser = new User(userName);
-                Users.Add(CurrentUser);               
+                Users.Add(CurrentUser);
                 IsNewUser = true;
                 Save();
             }
 
         }
-        
-        public void PasswordForLogin(string name,string password)
+
+
+
+        public void PasswordForLogin(string name, string password)
         {
             if (CurrentUser.Password == password)
             {
@@ -54,7 +55,7 @@ namespace InternetMarket.Controller
                 CurrentUser.Password = password;
             }
             try
-            {                     
+            {
                 while (CurrentUser.Password != password)
                 {
                     if (password.Length < 6)
@@ -71,9 +72,9 @@ namespace InternetMarket.Controller
                             Console.Write("Try Again:");
                             password = Console.ReadLine();
                         }
-                        
-                        
-                        
+
+
+
 
                     }
                 }
@@ -81,12 +82,49 @@ namespace InternetMarket.Controller
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
-                    
+
             }
-            
+
+        }
+
+        public void EmailUpdate(string email)
+        {          
+            CurrentUser.Email = email;
+            Save();
+        }
+        public void PasswordUpdate(string password) 
+        {
+            CurrentUser.Password = password;
+            Save();
+        }
+        public void NameUpdate(string name) 
+        {
+            CurrentUser.Name = name;
+            Save();
         }
 
 
+        
+
+        public void SetNewUserData(string user, DateTime birthDate, string password, string email)
+        {
+            CurrentUser.Name = user;
+            CurrentUser.DateOfBirth = birthDate;
+            CurrentUser.Email = email;
+            CurrentUser.Password = password;
+            Save();
+        }
+        
+
+        public void Save()
+        {
+            var formatter = new BinaryFormatter();
+
+            using (var fs = new FileStream("marketusers.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, Users);
+            };
+        }
         private List<User> GetUsersData()
         {
             var formatter = new BinaryFormatter();
@@ -105,24 +143,7 @@ namespace InternetMarket.Controller
             };
         }
 
-        public void SetNewUserData(string user, DateTime birthDate, string password, string email)
-        {
-            CurrentUser.Name = user;
-            CurrentUser.DateOfBirth = birthDate;
-            CurrentUser.Email = email;
-            CurrentUser.Password = password;
-            Save();
-        }
 
-        public void Save()
-        {
-            var formatter = new BinaryFormatter();
-
-            using (var fs = new FileStream("marketusers.dat", FileMode.OpenOrCreate))
-            {
-                formatter.Serialize(fs, Users);
-            };
-        }
 
     }
 }
---
+++ Model/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InternetMarket.Model
+{
+    public class Menu
+    {
+       
+        public void Interface() 
+        {
+            Thread.Sleep(1000);
+            Console.Clear();
+            MenuStart();
+            Console.WriteLine();
+            Console.Write("Choose a number or if you won't press\"s\":");
+   
+        }
+        public void MenuStart() 
+        {
+            Console.WriteLine("This is your profile");
+            Console.WriteLine("You could change each element.");
+            Console.WriteLine();
+            Console.WriteLine($"1.Name\t\t2.Password\t  3.Email");
+        }
+        
+    }
+}
