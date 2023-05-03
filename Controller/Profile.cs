4289903cbd6842e14cc0bf18474409403635a9c1
MaxPanenko <panenko123456789987654321@gmail.com>
2023-05-03 16:31:35 +0200

    Add project files.

---
+++ Controller/Profile.cs
@@ -0,0 +1,128 @@
+﻿using InternetMarket.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace InternetMarket.Controller
+{
+    public class Profile
+    {
+        public List<User> Users { get; }   
+        
+        public Dictionary <User,User> Emails { get; }
+
+        public User User { get; }
+
+        public User CurrentUser { get; }
+
+        public bool IsNewUser { get; }
+
+        public Profile(string userName) 
+        {
+            if (string.IsNullOrWhiteSpace(userName))
+            {
+                throw new ArgumentNullException("Name canno't be empty", nameof(userName));
+            }
+            Users = GetUsersData();
+
+            CurrentUser = Users.FirstOrDefault(u => u.Name == userName);
+
+            
+            
+
+            if (CurrentUser == null) 
+            {               
+                CurrentUser = new User(userName);
+                Users.Add(CurrentUser);               
+                IsNewUser = true;
+                Save();
+            }
+
+        }
+        
+        public void PasswordForLogin(string name,string password)
+        {
+            if (CurrentUser.Password == password)
+            {
+                Console.WriteLine("You are Log In System");
+                CurrentUser.Password = password;
+            }
+            try
+            {                     
+                while (CurrentUser.Password != password)
+                {
+                    if (password.Length < 6)
+                    {
+                        Console.WriteLine("Password canno't be less then 6 characters");
+                        Console.Write("Try Again:");
+                        password = Console.ReadLine();
+                    }
+                    else
+                    {
+                        if (CurrentUser.Password != password)
+                        {
+                            Console.WriteLine("Your password is incorrect");
+                            Console.Write("Try Again:");
+                            password = Console.ReadLine();
+                        }
+                        
+                        
+                        
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                    
+            }
+            
+        }
+
+
+        private List<User> GetUsersData()
+        {
+            var formatter = new BinaryFormatter();
+
+            using (var fs = new FileStream("MarketUsers.dat", FileMode.OpenOrCreate))
+            {
+                if (fs.Length > 0 && formatter.Deserialize(fs) is List<User> user)
+                {
+                    return user;
+                }
+                else
+                {
+                    return new List<User>();
+                }
+
+            };
+        }
+
+        public void SetNewUserData(string user, DateTime birthDate, string password, string email)
+        {
+            CurrentUser.Name = user;
+            CurrentUser.DateOfBirth = birthDate;
+            CurrentUser.Email = email;
+            CurrentUser.Password = password;
+            Save();
+        }
+
+        public void Save()
+        {
+            var formatter = new BinaryFormatter();
+
+            using (var fs = new FileStream("marketusers.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, Users);
+            };
+        }
+
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetMarket.Model
+{
+    [Serializable]
+
+
+    public class User
+    {
+        
+        public string Name { get; set; }
+       
+        public string Password { get; set;  }
+       
+        public DateTime DateOfBirth { get; set;  }
+       
+        public int Age { get { return DateTime.Now.Year - DateOfBirth.Year; } }
+        
+        public string Email { get; set; }
+
+        
+        public User(string name, string password, DateTime dateOfBirth, string email) 
+        {
+            #region Check statements
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Name canno't be empty.", nameof(name));
+            }
+
+            if (password == null)
+            {
+                throw new ArgumentNullException("Password canno't be empty.", nameof(password));
+            }
+
+            if (DateOfBirth < DateTime.Parse("01.01.1900") && DateOfBirth < DateTime.Now)
+            {
+                throw new ArgumentException("Impossible date of birth.", nameof(DateOfBirth));
+            }
+
+            if (Age < 0)
+            {
+                throw new ArgumentException("Age canno't be less more then 0.", nameof(Age));
+            }
+
+            if (Email == null)
+            {
+                throw new ArgumentException("Height canno't be less more then 130.", nameof(Email));
+            }
+
+            #endregion
+            Name = name;
+            Password = password;
+            DateOfBirth = dateOfBirth;
+            Email = email;
+            
+        }
+        public User() { }
+
+        public User(string name) 
+        {
+            if(string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("Name canno't be empty.", nameof(name));
+            }
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
